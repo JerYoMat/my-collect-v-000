74cc75cdee4da98a147346e0932289df01363241
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 22:46:18 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
-def my_collect(array)
-  i = 0 
-  collection = []
-  while i < array.length 
-    collection << yield(array[i])
-    i += 
-  end 
-end
+    def my_collect(array)
+      i = 0
+      collection = []
+      while i < array.length
+        collection << yield(array[i])
+        i += 1
+      end
+    end
 
